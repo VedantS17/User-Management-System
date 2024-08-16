45a329a822de01086461f00c5d2e87b573433bd1
Vedant Sarolkar <vedantsarolkar8761@gmail.com>
2024-08-16 19:37:50 +0530

    added

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import User from "./components/getuser/User.jsx";
+import Add from "./components/adduser/Add.jsx";
+import Edit from "./components/updateuser/Edit.jsx";
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path: "/add",
+      element: <Add />,
+    },
+    {
+      path: "/edit/:id",
+      element: <Edit />,
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={route}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
